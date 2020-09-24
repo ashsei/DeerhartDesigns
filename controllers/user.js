533644a4a8cb62a7957b4f3e477f76bdbb723652
ashsei <58061442+ashsei@users.noreply.github.com>
2020-09-24 17:51:03 -0600

    User Signin Method Created

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 const {errorHandler} = require('../helpers/dbErrorHandler.js')
 
 exports.signup = (req, res) => {
@@ -17,3 +19,23 @@ exports.signup = (req, res) => {
         });
     });
 };
+
+exports.signin = (req, res) => {
+    const {email, password} = req.body;
+    User.findOne({email}, (err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: "User with that email does not exist. Please ensure you have registered."
+            })
+        };
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: 'Email and password do not match. Please try again.'
+            })
+        }
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+        res.cookie('t', token, {expire: new Date() + 9999});
+        const {_id, name, email, role} = user;
+        return res.json({token, user: {_id, name, email, role}});
+    });
+}
