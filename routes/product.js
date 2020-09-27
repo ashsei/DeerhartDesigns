4686e396dd24ed9e8b04413a2ceeadf8078e8239
ashsei <58061442+ashsei@users.noreply.github.com>
2020-09-26 19:34:11 -0600

    Product List Routes and Assoc Middleware Established and Implemented

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update } = require('../controllers/product')
+const { create, productById, read, remove, update, list, listRelated } = require('../controllers/product')
 const{ requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
 
+router.get('/products', list)
+router.get('/products/related/:productId', listRelated)
 router.get('/product/:productId', read)
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
